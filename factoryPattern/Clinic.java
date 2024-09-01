3b1e81ad35f9415302a5299ea31630c7f079d89f
Pia Katleya V. Macalanda <130662592+PiaMacalanda@users.noreply.github.com>
2024-09-01 08:27:55 +0000

    Dsiplay lives and breed

--- factoryPattern/Clinic.java
+++ factoryPattern/Clinic.java
@@ -21,19 +21,29 @@ public class Clinic {
                 petFile.setPetName("Bantay");
                 petFile.setPet(pet);
                 ((Dog) pet).setBreed("German Shepperd");
+
+                // To display dog's breed
+                System.out.println("Breed: " + ((Dog) pet).getBreed());
                 break;
+
             case 2: pet = new Cat();
                 petFile.setPetId("C01");
                 petFile.setPetName("Muning");
                 petFile.setPet(pet);
                 ((Cat) pet).setNoOfLives(9);
+                System.out.println("Number of lives: " + ((Cat) pet).getNoOfLives());
         }
 
-        System.out.println("Pet id is " + petFile.getPetId());
-        System.out.println("Pet name is " + petFile.getPetName());
-        System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
-        System.out.println("Communication sound: "+ petFile.getPet().makeSound());
-        System.out.println("Play mode: " + petFile.getPet().play());
-
+                displayPetDetails(petFile);
+                input.close();
     }
+
+        public static void displayPetDetails(PetRecord petFile){
+
+            System.out.println("Pet id is " + petFile.getPetId());
+            System.out.println("Pet name is " + petFile.getPetName());
+            System.out.println("Pet kind: " + petFile.getPet().getClass().getSimpleName());
+            System.out.println("Communication sound: "+ petFile.getPet().makeSound());
+            System.out.println("Play mode: " + petFile.getPet().play());
+        }
 }
